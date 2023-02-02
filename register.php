83765b088022b8c292762b067abaf1b7b1f4bafa
Coen <cbrumleger@gmail.com>
2023-02-02 12:27:14 +0100

    v1.1.2: - register.php commentaar toegevoegd

--- register.php
+++ register.php
@@ -1,23 +1,23 @@
-<?php
-// Include config file
-$link = "";
+<?php /** @noinspection PhpParamsInspection */
+// Eerst $link aanmaken en daarna doormiddel van config.php $link een waarde geven
+$link = '';
 require_once "config.php";
 
-// Define variables and initialize with empty values
+// Zorgen dat variabelen worden gedifineerd
 $username = $password = $confirm_password = "";
 $username_err = $password_err = $confirm_password_err = "";
 
 
-// Processing form data when form is submitted
+// Deze functie wordt aangeslagen als de form ingeleverd wordt
 if($_SERVER["REQUEST_METHOD"] == "POST"){
 
-    // Validate username
-    if(empty(trim($_POST["username"]))){
+    // Kijkt of de gebruikersnaam (in dit geval email) een goede email is
+    if(empty(trim($_POST["username"]))){ // kijkt of de email leeg is
         $username_err = "Please enter an email.";
-    } elseif(!filter_var(trim($_POST["username"]), FILTER_VALIDATE_EMAIL)){
+    } elseif(!filter_var(trim($_POST["username"]), FILTER_VALIDATE_EMAIL)){ // kijkt of het een email is
         $username_err = "Email is not an email.";
     } else{
-        // Prepare a select statement
+        // Maak een SQL
         $sql = "SELECT id FROM users WHERE email = ?";
 
         if($stmt = mysqli_prepare($link, $sql)){
@@ -98,16 +98,17 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="nl">
 <head>
-    <title>Register | Freshie</title>
+    <title>Register | Freshie</title> <!-- Titel instellen van de pagina -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="style.css">
-    <style>
-        body {font-family: "Times New Roman", Georgia, Serif;}
+    <link rel="stylesheet" href="style.css"> <!-- style.css gebruiken voor de opmaak van de pagina -->
+    <style> /* Extra opmaak specifiek voor deze pagina die niet bij de rest van de pagina hoort*/
+        body {
+            font-family: "Times New Roman", Georgia, Serif, serif;}
         h1, h2, h3, h4, h5, h6 {
-            font-family: "Playfair Display";
+            font-family: "Playfair Display", serif;
             letter-spacing: 5px;
         }
         input[type=text], input[type=password] {
@@ -140,7 +141,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             transform: translateY(35%);
         }
     </style>
-    <link rel="icon" type="image/x-icon" href="photo\freshie%20logo.png">
+    <link rel="icon" type="image/x-icon" href="photo/freshie%20logo.png"> <!-- Logo instellen -->
 </head>
 <body>
 <!-- Header -->
@@ -148,6 +149,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     <div class="bar white padding card" style="letter-spacing: 4px;">
         <div class="left">
             <a href="index.html" class="button">
+                <!--suppress CheckImageSize -->
                 <img class="image" src="photo/freshie logo.png" alt="Freshie Logo" width="50" height="50">
             </a>
         </div>
@@ -165,7 +167,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 <div class="content" style="max-width: 1100px">
     <div class="row padding-64" id="registreren">
         <div class="right col m6 padding-large hide-small">
-            <img src="photo\freshie%20registreren.jpg" class="round image opacity-min" alt="Voorbeeld eten" width="600" height="750">
+            <img src="photo/freshie%20registreren.jpg" class="round image opacity-min" alt="Voorbeeld eten" width="600" height="750">
         </div>
 
         <div class="left col m6 padding-large">
@@ -173,13 +175,19 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                 <h2 class="middle">Registreren</h2>
                 <div class="container">
                     <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-                        <input type="text" name="username" placeholder="Email" class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>">
+                        <label>
+                            <input type="text" name="username" placeholder="Email" class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>">
+                        </label>
                         <span class="invalid-feedback"><?php echo $username_err; ?></span>
 
-                        <input type="password" name="password" placeholder="Wachtwoord" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $password; ?>">
+                        <label>
+                            <input type="password" name="password" placeholder="Wachtwoord" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $password; ?>">
+                        </label>
                         <span class="invalid-feedback"><?php echo $password_err; ?></span>
 
-                        <input type="password" name="confirm_password" placeholder="Bevestig wachtwoord" class="form-control <?php echo (!empty($confirm_password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $confirm_password; ?>">
+                        <label>
+                            <input type="password" name="confirm_password" placeholder="Bevestig wachtwoord" class="form-control <?php echo (!empty($confirm_password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $confirm_password; ?>">
+                        </label>
                         <span class="invalid-feedback"><?php echo $confirm_password_err; ?></span>
 
                         <button type="submit">Registreren</button>
