c6674362924df55855d2aa6b65e642c94f99ac8f
coenbrummelhuis <cbrumleger@gmail.com>
2023-03-20 13:37:02 +0100

    v1.2.0: - settings.php gemaakt en werkend gemaakt

--- settings.php
+++ settings.php
@@ -7,6 +7,84 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     header("location: login.php");
     exit;
 }
+$link = "";
+$geenVlees_status = false;
+$geenLactose_status = false;
+$geenGluten_status = false;
+
+require_once "config.php";
+
+$sql = "SELECT voorkeuren FROM users WHERE ID = ?";
+if($stmt = mysqli_prepare($link, $sql)){
+    mysqli_stmt_bind_param($stmt, "i", $param_id);
+    $param_id = $_SESSION["id"];
+
+    if(mysqli_stmt_execute($stmt)){
+        mysqli_stmt_store_result($stmt);
+
+        mysqli_stmt_bind_result($stmt, $voorkeuren);
+        if(mysqli_stmt_fetch($stmt)){
+            $voorkeuren_list_array = unserialize($voorkeuren);
+            foreach ($voorkeuren_list_array as $value){
+                if($value == "geenVlees"){
+                    $geenVlees_status = true;
+                }else if($value == "geenLactose"){
+                    $geenLactose_status = true;
+                }else if($value == "geenGluten"){
+                    $geenGluten_status = true;
+                }
+            }
+        }
+    }
+}
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $geenVlees_status = false;
+    $geenLactose_status = false;
+    $geenGluten_status = false;
+
+    $voorkeuren_list = array($_POST['geenVlees'], $_POST['geenLactose'], $_POST['geenGluten']);
+//    foreach ($voorkeuren_list as $value) {
+//        echo $value . "<br>";
+//    }
+//    echo serialize($voorkeuren_list) . "<br>";
+
+    $sql = "UPDATE users SET voorkeuren = ? WHERE id = ?";
+    if($stmt = mysqli_prepare($link, $sql)){
+        mysqli_stmt_bind_param($stmt, "si", $param_voorkeuren, $param_id);
+
+        $param_voorkeuren = serialize($voorkeuren_list);
+        $param_id = $_SESSION["id"];
+
+        mysqli_stmt_execute($stmt);
+
+        mysqli_stmt_close($stmt);
+    }
+
+    $sql = "SELECT voorkeuren FROM users WHERE ID = ?";
+    if($stmt = mysqli_prepare($link, $sql)){
+        mysqli_stmt_bind_param($stmt, "i", $param_id);
+        $param_id = $_SESSION["id"];
+
+        if(mysqli_stmt_execute($stmt)){
+            mysqli_stmt_store_result($stmt);
+
+            mysqli_stmt_bind_result($stmt, $voorkeuren);
+            if(mysqli_stmt_fetch($stmt)){
+                $voorkeuren_list_array = unserialize($voorkeuren);
+                foreach ($voorkeuren_list_array as $value){
+                    if($value == "geenVlees"){
+                        $geenVlees_status = true;
+                    }else if($value == "geenLactose"){
+                        $geenLactose_status = true;
+                    }else if($value == "geenGluten"){
+                        $geenGluten_status = true;
+                    }
+                }
+            }
+        }
+    }
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -86,20 +164,23 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
             <form method="post">
                 <p>
                     <label>
-                        <input type="checkbox" name="geenVlees">    Ik wil geen vlees
+                        <input type="hidden" name="geenVlees" value="">
+                        <input type="checkbox" name="geenVlees" value="geenVlees" <?php if ($geenVlees_status) echo 'checked'; ?> >    Ik wil geen vlees
                     </label>
                 </p>
                 <p>
                     <label>
-                        <input type="checkbox" name="geenLactose">  Ik wil geen lactose
+                        <input type="hidden" name="geenLactose" value="">
+                        <input type="checkbox" name="geenLactose" value="geenLactose" <?php if ($geenLactose_status) echo 'checked'; ?> >  Ik wil geen lactose
                     </label>
                 </p>
                 <p>
                     <label>
-                        <input type="checkbox" name="geenGluten">   Ik wil geen gluten
+                        <input type="hidden" name="geenGluten" value="">
+                        <input type="checkbox" name="geenGluten" value="geenGluten" <?php if ($geenGluten_status) echo 'checked'; ?> >   Ik wil geen gluten
                     </label>
                 </p>
-                <button type="submit">Bevestig voorkeuren</button>
+                <button type="submit">Bevestig</button>
             </form>
             <div class="niet-geregistreerd middle">
                 <a href="unsubscribe.php">Uitschrijven</a>
