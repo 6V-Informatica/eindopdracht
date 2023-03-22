0a2fc9ea91ffcfe682d4b542ada802545f453835
Coen <cbrumleger@gmail.com>
2023-03-22 14:21:03 +0100

    v1.3.5: - Added Schema-generator.php to the Jurre-branch

--- Schema-generator.php
+++ Schema-generator.php
@@ -4,27 +4,73 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
     header("location: login.php");
     exit;
 }
+
 $link = '';
+$geenVlees_status = false;
+$geenLactose_status = false;
+$geenGluten_status = false;
+
 require_once "config.php";
-$geenVleesRecepten = rand(1,14);
-$geenFilterRecepten = rand(15,28);
-$geenLactoseRecepten = rand(29,42);
-$geenGlutenRecepten = rand(,);
+
+$sql = "SELECT voorkeuren FROM users WHERE ID = ?";
+if($stmt = mysqli_prepare($link, $sql)){
+    mysqli_stmt_bind_param($stmt, "i", $param_id);
+    $param_id = $_SESSION["id"];
+
+    if(mysqli_stmt_execute($stmt)){
+        mysqli_stmt_store_result($stmt);
+        mysqli_stmt_bind_result($stmt, $voorkeuren_serialized);
+        if(mysqli_stmt_fetch($stmt)){
+            $voorkeuren = unserialize($voorkeuren_serialized);
+            foreach ($voorkeuren as $value){
+                if($value == "geenVlees"){
+                    $geenVlees_status = true;
+                }else if($value == "geenLactose"){
+                    $geenLactose_status = true;
+                }else if($value == "geenGluten"){
+                    $geenGluten_status = true;
+                }
+            }
+        }
+    }
+    mysqli_stmt_close($stmt);
+}
+
+//$geenVleesRecepten = rand(1,14);
+//$geenFilterRecepten = rand(15,28);
+//$geenLactoseRecepten = rand(29,42);
+//$geenGlutenRecepten = rand(,);
 // Nog extra recepten
 
-voorkeuren = list($voorkeuren);
-vleesVoorkeur = voorkeuren[0];
-lactoseVoorkeur = voorkeuren[1];
-glutenVoorkeur = voorkeuren[2];
+if($geenVlees_status){
+    $numbers = range(1,14);
+    shuffle($numbers);
 
-$numbers = array();
-while (count($numbers) < 14){
-    $random = rand(1,14);
-    if (!in_array($random, $numbers)) {
-        $numbers[] = $random;
-    }
+}else if($geenLactose_status){
+    $numbers = range(29,42);
+    shuffle($numbers);
+
+}else if($geenGluten_status){
+    $numbers = range(1,14);
+    shuffle($numbers);
+}else {
+    $numbers = range(15,28);
+    shuffle($numbers);
 }
-$sql = "UPDATE users SET schema = ? WHERE id = ?";
+
+//voorkeuren = list($voorkeuren);
+//vleesVoorkeur = voorkeuren[0];
+//lactoseVoorkeur = voorkeuren[1];
+//glutenVoorkeur = voorkeuren[2];
+//
+//$numbers = array();
+//while (count($numbers) < 14){
+//    $random = rand(1,14);
+//    if (!in_array($random, $numbers)) {
+//        $numbers[] = $random;
+//    }
+//}
+$sql = "UPDATE users SET weekschema = ? WHERE id = ?";
 if($stmt= mysqli_prepare($link, $sql)) {
     mysqli_stmt_bind_param($stmt, "si", $param_schema, $param_id);
     $param_schema = serialize($numbers);
@@ -33,13 +79,7 @@ if($stmt= mysqli_prepare($link, $sql)) {
         header("location: welcome.php");
         exit();
     } else {
-        echo "Er is iets verkeerd gegaan probeer het later opnieuw."
+        echo "Er is iets verkeerd gegaan probeer het later opnieuw.";
     }
     mysqli_stmt_close($stmt);
 }
-
-
-
-
-
-?>
